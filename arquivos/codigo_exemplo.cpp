6f5676b60ccbbfb92a3d0d4f9f0e27d092b465b7
Enzo Lisboa <lisboaenzo@gmail.com>
2025-10-30 19:03:29 -0300

    first commit

---
+++ arquivos/codigo_exemplo.cpp
@@ -0,0 +1,17 @@
+// exemplo de código em linguagem C++ 
+// para somar dois números
+#include <iostream>
+using namespace std;
+
+int main() {
+  double num1, num2;
+  cout << "Digite o primeiro numero: ";
+  cin >> num1;
+  cout << "Digite o segundo numero: ";
+  cin >> num2;
+
+  double soma = num1 + num2;
+
+  cout << "A soma de "<< num1 << " e " << num2 << "=" << soma << endl;
+  return 0;
+}
